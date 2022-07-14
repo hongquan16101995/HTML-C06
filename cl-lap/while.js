049bc4e98f58699406a3e232807b44dd17a90370
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-07-14 15:09:32 +0700

    vòng lặp

---
+++ cl-lap/while.js
@@ -0,0 +1,11 @@
+let count = 0
+while (count++ < 10) {
+    count++;
+}
+console.log("Count 1:" + count)
+
+let count1 = 0
+do {
+    count1++;
+} while (count1++ < 10)
+console.log("Count 2:" + count1)
